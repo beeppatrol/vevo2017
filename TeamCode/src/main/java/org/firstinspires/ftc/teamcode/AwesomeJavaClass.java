e49443abad771f1b5fcb73960e5ad5473f6f7ff4
beeppatrol <beeppatrol@gmail.com>
2017-02-09 20:36:42 -0700

    test commit from Perkins' machine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AwesomeJavaClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AwesomeJavaClass.java
@@ -7,6 +7,7 @@ public class AwesomeJavaClass {
 
     public void SeanIsAwesome () {
         // Wes might be...  we'll see!! Bananas aren't potatos but the world has ended.
+        // somebody can't spell potatoes
     }
 
 }
