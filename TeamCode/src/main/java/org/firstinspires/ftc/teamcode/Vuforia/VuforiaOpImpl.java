83fb8482d29e7426be581bf86b5baee0f7d29238
ssankey <sean.d.sankey@aexp.com>
2017-08-03 21:08:18 -0700

    added VuforiaOpImpl class to navigate to objects using Vuforia (Sean 8/3/2017)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia/VuforiaOpImpl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia/VuforiaOpImpl.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.Vuforia;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.vuforia.HINT;
 import com.vuforia.Vuforia;
 
@@ -22,6 +24,7 @@ import org.firstinspires.ftc.teamcode.R;
     https://www.youtube.com/watch?v=2z-o9Ts8XoE
     see 15:40 for demo of code working below
 
+    https://github.com/FIXIT3491
     TODO: add methods/class to navigate to the tracked object
 
  */
@@ -29,6 +32,13 @@ public class VuforiaOpImpl extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        DcMotor driveR = hardwareMap.dcMotor.get("driveR"); // change to ours, driveR = right drive
+        driveR.setDirection(DcMotorSimple.Direction.REVERSE);
+        driveR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        DcMotor driveL = hardwareMap.dcMotor.get("driveL"); // change to ours, driveL = left drive
+        driveL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK; // back or front camera
@@ -43,7 +53,98 @@ public class VuforiaOpImpl extends LinearOpMode {
         beacons.setName("gears");
 
         waitForStart();
+
+        VuforiaTrackableDefaultListener wheels = (VuforiaTrackableDefaultListener)beacons.get(0).getListener();
         beacons.activate();  // vuforia starts tracking for objects (no point calling this until after waitForStart() )
+
+
+        // *** START tracking code  ** //
+
+            driveL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            driveR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            driveL.setPower(0.2);
+            driveR.setPower(0.2);
+
+            while (opModeIsActive() && wheels.getRawPose() == null ) {
+                idle();
+                // robot can't see beacons while in this loop, exit loop when target/beacon found
+            }
+
+            driveL.setPower(0.0);
+            driveR.setPower(0.0);
+
+            // stop (above) and analyze beacon here...
+
+            VectorF angles = anglesFromTarget(wheels);
+            VectorF trans = navOffWall(wheels.getPose().getTranslation(), Math.toDegrees(angles.get(0))-90, new VectorF(500,0,0));
+            if(trans.get(0)>0) {
+                driveL.setPower(0.02);
+                driveR.setPower(-0.02);
+            } else {
+                // other direction
+                driveL.setPower(-0.02);
+                driveR.setPower(0.02);
+            }
+
+            do {
+                if (wheels.getPose()!=null){
+                    trans = navOffWall(wheels.getPose().getTranslation(), Math.toDegrees(angles.get(0))-90, new VectorF(500,0,0));
+                }
+                idle();
+            } while (opModeIsActive() && Math.abs(trans.get(0)) > 30);
+
+            // stop
+            driveL.setPower(0.0);
+            driveR.setPower(0.0);
+
+            // use encoders to drive forward so that we're in front of the beacon
+            driveL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            driveR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // 150 value below will change depending on our/your robot
+            // https://www.youtube.com/watch?v=qDoLmZyH69o, see 10:27 for more information
+            // ...  150) / 409.575 * 560 ...  150 value is due to phone being 15cm (150mm) off-center on robot
+            // 409.575 is wheel circumference
+            // 560 ticks per rotation for Andy Mark 20's,  1120 ticks per rotation for Andy Mark 40's
+            driveL.setTargetPosition((int) (driveL.getCurrentPosition() + ((Math.hypot(trans.get(0), trans.get(2)) + 150) / 409.575 * 1120 )));
+            driveR.setTargetPosition((int) (driveL.getCurrentPosition() + ((Math.hypot(trans.get(0), trans.get(2)) + 150) / 409.575 * 1120 )));
+
+            driveL.setPower(0.3);
+            driveR.setPower(0.3);
+
+            while(opModeIsActive() && driveL.isBusy() && driveR.isBusy()) {
+                idle();
+            }
+
+            driveL.setPower(0.0);
+            driveR.setPower(0.0);
+
+            driveL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            driveR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            while(opModeIsActive() && (wheels.getPose()==null || Math.abs(wheels.getPose().getTranslation().get(0)) > 10)) {
+                if(wheels.getPose()!=null) {
+                    if(wheels.getPose().getTranslation().get(0) > 0 ){
+                        driveL.setPower(-0.3);
+                        driveR.setPower(0.3);
+                    } else { // other way, adjusting for over correction
+                        driveL.setPower(0.3);
+                        driveR.setPower(-0.3);
+                    }
+                } else {
+                    driveL.setPower(-0.3);
+                    driveR.setPower(0.3);
+                }
+            }
+
+            // robot should be stopped and looking at the beacon now...
+            driveL.setPower(0);
+            driveR.setPower(0);
+
+        // *** END tracking code  ** //
+
+
 
         while(opModeIsActive()) {
             for (VuforiaTrackable beacon : beacons) {
@@ -62,6 +163,22 @@ public class VuforiaOpImpl extends LinearOpMode {
             telemetry.update();
         }
 
+    }
+
+
+    public VectorF navOffWall(VectorF trans, double robotAngle, VectorF offWall){
+        return new VectorF((float) (trans.get(0) - offWall.get(0) * Math.sin(Math.toRadians(robotAngle)) - offWall.get(2) * Math.cos(Math.toRadians(robotAngle))), trans.get(1), (float) (trans.get(2) + offWall.get(0) * Math.cos(Math.toRadians(robotAngle)) - offWall.get(2) * Math.sin(Math.toRadians(robotAngle))));
+    }
+
+    public VectorF anglesFromTarget(VuforiaTrackableDefaultListener image){
+
+        float [] data = image.getRawPose().getData();
+        float [] [] rotation = {{data[0], data[1]}, {data[4], data[5], data[6]}, {data[8], data[9], data[10]}};
+        double thetaX = Math.atan2(rotation[2][1], rotation[2][2]);
+        double thetaY = Math.atan2(-rotation[2][0], Math.sqrt(rotation[2][1] * rotation[2][1] + rotation[2][2] * rotation[2][2]));
+        double thetaZ = Math.atan2(rotation[1][0], rotation[0][0]);
+
+        return new VectorF((float)thetaX, (float)thetaY, (float)thetaZ);
     }
 
     private static String getLicenseKey () {
